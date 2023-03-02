9fc2a8e783cb0f24962d0cf5efe74101d0358b88
Abdelrazek Emad <53626803+AbdelrazekEmad@users.noreply.github.com>
2023-03-02 17:48:26 +0200

    upload website

---
+++ content/Js/owl.carousel-ltr.js
@@ -0,0 +1,15 @@
+$(".owl-carousel-hero").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: false,
+  dots: false,
+  autoplay: true,
+  animateOut: "fadeOut",
+  animateIn: "fadeIn",
+  autoplaySpeed: 6000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
